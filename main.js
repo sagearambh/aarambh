006410cb93914e94041843c859fda962e51bed4a
sagearambh <sagearambh@gmail.com>
2024-05-30 22:31:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+const syscPointer =({x: pointerX, y: pointerY})=>{
+
+    const x = pointerX.toFixed(2)
+
+    const y = pointerY.toFixed(2)
+
+    const xp = (pointerX / window.innerWidth).toFixed(2)
+
+    const yp = (pointerY / window.innerWidth).toFixed(2)
+
+    document.documentElement.style.setProperty('--x',x)
+
+    document.documentElement.style.setProperty('--xp',xp)
+
+    document.documentElement.style.setProperty('--y',y)
+
+    document.documentElement.style.setProperty('--yp',yp)
+
+}
+
+document.body.addEventListener('pointermove' , syscPointer)
